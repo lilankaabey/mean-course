27d08253894e3adc1978ae4a7ab8874eb0a1811a
Lilanka Ravinath <lilankaravinath@outlook.com>
2018-11-03 18:39:08 +0530

    Change angular-cli 1.7.3 to 7.0.4 and I have to change whole project thats why this commit be intiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-
 
 @NgModule({
   declarations: [
