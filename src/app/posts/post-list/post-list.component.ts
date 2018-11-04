e24c1f6f62de2505685dab6a4de06178de7d3b38
Lilanka Ravinath <lilankaravinath@outlook.com>
2018-11-04 17:36:32 +0530

    Get Posts from Post-Create to Post-List : Its kind of dependency injection using service

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -16,4 +17,9 @@ export class PostListComponent {
   // ];
 
   @Input() posts: Post[] = [];
+  // postsService: PostsService;
+
+  constructor(public postsService: PostsService) {
+    // this.postsService = postsService;
+  }
 }
