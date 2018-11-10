89a2e5f21343c0028a8338ad8ed708dbe222640c
Lilanka Ravinath <lilankaravinath@outlook.com>
2018-11-10 12:41:36 +0530

    Add Post with an ID

--- backend/app.js
+++ backend/app.js
@@ -35,9 +35,11 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
@@ -52,9 +54,9 @@ app.get('/api/posts', (req, res, next) => {
 
 app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then(result => {
-    console.log(result)});
-  // console.log(req.params.id);
-  res.status(200).json({ message: "Post Deleted!"});
+    console.log(result);
+    res.status(200).json({ message: "Post Deleted!"});
+  });
 });
 
 module.exports = app;
