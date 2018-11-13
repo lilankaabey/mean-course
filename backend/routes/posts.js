86f2cc0fe10153a59d0886da5aaf0d49f7f2e4a3
Lilanka Ravinath <lilankaravinath@outlook.com>
2018-11-13 15:34:50 +0530

    Work on the Pagination Backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -69,7 +69,15 @@ router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) 
 });
 
 router.get("", (req, res, next) => {
-  Post.find().then(documents => {
+  const pageSize = +req.query.pagesize; // Queries values come as string we add + to convert it to numeric
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  if (pageSize && currentPage) {
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery.then(documents => {
     res.status(200).json({
       message: 'Posts fetched succesfully!',
       posts: documents
