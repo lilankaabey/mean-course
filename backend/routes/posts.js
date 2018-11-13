a0317664cee7c1a37d9a606bebe9fd20884ecd45
Lilanka Ravinath <lilankaravinath@outlook.com>
2018-11-13 17:13:38 +0530

    Fetch Posts Correctly

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -72,15 +72,21 @@ router.get("", (req, res, next) => {
   const pageSize = +req.query.pagesize; // Queries values come as string we add + to convert it to numeric
   const currentPage = +req.query.page;
   const postQuery = Post.find();
+  let fetchedPosts;
   if (pageSize && currentPage) {
     postQuery
       .skip(pageSize * (currentPage - 1))
       .limit(pageSize);
   }
   postQuery.then(documents => {
+    fetchedPosts = document;
+    return Post.count();
+  })
+  .then(count => {
     res.status(200).json({
       message: 'Posts fetched succesfully!',
-      posts: documents
+      posts: fetchedPosts,
+      maxPosts: count
     });
   });
 });
