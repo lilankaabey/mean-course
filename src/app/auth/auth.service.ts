ce722730ae637220f53360b4114ef25e32da5ad8
Lilanka Ravinath <inbox.lilanka@gmail.com>
2018-11-18 20:15:22 +0530

    Improve the UI Header to Reflect the Authentication Status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 import { AuthData } from './auth-data.model';
 
@@ -8,26 +9,33 @@ import { AuthData } from './auth-data.model';
 
 export class AuthService {
   private token: string;
-    constructor(private http: HttpClient) {}
+  private authStatusListener = new Subject<boolean>();
 
-    getToken() {
-      return this.token;
-    }
+  constructor(private http: HttpClient) {}
 
-    createUser(email: string, password: string) {
-        const authData: AuthData = { email: email, password: password };
-        this.http.post('http://localhost:3000/api/user/signup', authData)
-            .subscribe(response => {
-                console.log(response);
-            });
-    }
+  getToken() {
+    return this.token;
+  }
 
-    login(email: string, password: string) {
-        const authData: AuthData = { email: email, password: password };
-        this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
-            .subscribe(response => {
-                const token = response.token;
-                this.token = token;
-            });
-    }
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
+
+  createUser(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
+    this.http.post('http://localhost:3000/api/user/signup', authData)
+        .subscribe(response => {
+            console.log(response);
+        });
+  }
+
+  login(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+        .subscribe(response => {
+            const token = response.token;
+            this.token = token;
+            this.authStatusListener.next(true);
+        });
+  }
 }
