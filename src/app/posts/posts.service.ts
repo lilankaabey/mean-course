f2c95bf811db394ba67aa19f80857239e6a14f6e
Lilanka Ravinath <inbox.lilanka@gmail.com>
2018-12-01 19:27:54 +0530

    Use the User ID on the Frontend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -44,7 +44,12 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string, title: string, content: string, imagePath: string }>('http://localhost:3000/api/posts/' + id);
+    return this.http.get<{
+      _id: string,
+      title: string,
+      content: string,
+      imagePath: string,
+      creator: string }>('http://localhost:3000/api/posts/' + id);
   }
 
   addPost(title: string, content: string, image: File) {
@@ -71,7 +76,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
